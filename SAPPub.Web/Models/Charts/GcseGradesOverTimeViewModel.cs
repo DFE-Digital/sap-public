4b122f774421046ce0c41b2f9a0ebff95618fe88
GADDAM, Rajesh <Rajesh.GADDAM@EDUCATION.GOV.UK>
2025-11-06 09:37:11 +0000

    Poc Charts - Added chart controller - Added view models related to charts - Added views for different chart - Changes to layout to show the charts link

---
+++ SAPPub.Web/Models/Charts/GcseGradesOverTimeViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SAPPub.Web.Models.Charts;
+
+public class GcseGradesOverTimeViewModel
+{
+    public required List<string> Years { get; set; }
+
+    public required List<double> School { get; set; }
+
+    public required List<double> Sheffield { get; set; }
+
+    public required List<double> England { get; set; }
+}
---
+++ SAPPub.Web/Models/Charts/GcseGradesViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace SAPPub.Web.Models.Charts;
+
+public class GcseGradesViewModel
+{
+    public double SchoolGirls { get; set; }
+    public double SheffieldGirls { get; set; }
+    public double EnglandGirls { get; set; }
+
+    public double SchoolBoys { get; set; }
+    public double SheffieldBoys { get; set; }
+    public double EnglandBoys { get; set; }
+
+    public double SchoolSen { get; set; }
+    public double SheffieldSen { get; set; }
+    public double EnglandSen { get; set; }
+}
