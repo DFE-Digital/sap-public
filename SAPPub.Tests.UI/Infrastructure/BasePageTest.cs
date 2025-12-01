d7e9f4bbda3b3db1a9d8aa68bfec6ccb8d2f6d61
GKAHLON <gurpal.kahlon@education.gov.uk>
2025-12-01 12:01:56 +0000

    Added conditions to save screenshots, traces and vides only on test failures

--- SAPPub.Tests.UI/Infrastructure/BasePageTest.cs
+++ SAPPub.Tests.UI/Infrastructure/BasePageTest.cs
@@ -1,61 +1,73 @@
 ﻿using Microsoft.Playwright;
 using Microsoft.Playwright.Xunit;
 
-namespace SAPPub.Tests.UI.Infrastructure
+namespace SAPPub.Tests.UI.Infrastructure;
+
+[Collection("Playwright Tests")]
+public class BasePageTest : PageTest
 {
-    [Collection("Playwright Tests")]
-    public class BasePageTest : PageTest
+    private bool _testFailed = false;
+
+    public override BrowserNewContextOptions ContextOptions()
     {
-        public override BrowserNewContextOptions ContextOptions()
+        return new BrowserNewContextOptions
         {
-            return new BrowserNewContextOptions
-            {
-                IgnoreHTTPSErrors = true,
-                RecordVideoDir = "SAPPub.Tests.UI/test-artifacts/videos",
-                RecordVideoSize = new() { Width = 1280, Height = 720 }
-            };
-        }
+            IgnoreHTTPSErrors = true,
+            RecordVideoDir = "SAPPub.Tests.UI/test-artifacts/videos",
+            RecordVideoSize = new() { Width = 1280, Height = 720 }
+        };
+    }
 
-        public override async Task InitializeAsync()
+    public override async Task InitializeAsync()
+    {
+        Directory.CreateDirectory("SAPPub.Tests.UI/test-artifacts/screenshots");
+        Directory.CreateDirectory("SAPPub.Tests.UI/test-artifacts/traces");
+        Directory.CreateDirectory("SAPPub.Tests.UI/test-artifacts/videos");
+
+        await base.InitializeAsync();
+
+        await Context.Tracing.StartAsync(new()
         {
-            Directory.CreateDirectory("SAPPub.Tests.UI/test-artifacts/screenshots");
-            Directory.CreateDirectory("SAPPub.Tests.UI/test-artifacts/traces");
-            Directory.CreateDirectory("SAPPub.Tests.UI/test-artifacts/videos");
+            Screenshots = true,
+            Snapshots = true,
+            Sources = true
+        });
+    }
 
-            await base.InitializeAsync();
+    public override async Task DisposeAsync()
+    {
+        var id = Guid.NewGuid().ToString();
 
-            await Context.Tracing.StartAsync(new()
-            {
-                Screenshots = true,
-                Snapshots = true,
-                Sources = true
-            });
-        }
-
-        public override async Task DisposeAsync()
+        try
         {
-            string id = Guid.NewGuid().ToString();
-
+            // Try to stop tracing (will succeed only on passed tests)
             await Context.Tracing.StopAsync(new()
             {
                 Path = $"SAPPub.Tests.UI/test-artifacts/traces/{id}.zip"
             });
+
+            // Passed test → save nothing else
+        }
+        catch
+        {
+            // Failed test → mark flag & save screenshot
+            _testFailed = true;
 
             await Page.ScreenshotAsync(new()
             {
                 Path = $"SAPPub.Tests.UI/test-artifacts/screenshots/{id}.png",
                 FullPage = true
             });
-
-            await base.DisposeAsync();
         }
 
-        protected Task<IResponse?> GoToPageAysnc(string relativeUrl)
-        {
-            var baseUrl = Environment.GetEnvironmentVariable("BASE_URL")
-                ?? "https://localhost:3000";
+        await base.DisposeAsync();
+    }
 
-            return Page.GotoAsync($"{baseUrl}/{relativeUrl}");
-        }
+    protected async Task<IResponse?> GoToPageAysnc(string relativeUrl)
+    {
+        var baseUrl = Environment.GetEnvironmentVariable("BASE_URL")
+            ?? "https://localhost:3000";
+
+        return await Page.GotoAsync($"{baseUrl}/{relativeUrl}");
     }
 }
