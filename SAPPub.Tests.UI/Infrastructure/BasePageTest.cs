6fc3d909464b513b774897a981c8d606ebd84ecf
GKAHLON <gurpal.kahlon@education.gov.uk>
2025-12-01 11:14:23 +0000

    Updated basepagetest

--- SAPPub.Tests.UI/Infrastructure/BasePageTest.cs
+++ SAPPub.Tests.UI/Infrastructure/BasePageTest.cs
@@ -1,17 +1,11 @@
 ﻿using Microsoft.Playwright;
 using Microsoft.Playwright.Xunit;
-using Xunit;
 
 namespace SAPPub.Tests.UI.Infrastructure
 {
-    // Required to disable parallel test execution
     [Collection("Playwright Tests")]
-    public class BasePageTest : PageTest, IAsyncLifetime
+    public class BasePageTest : PageTest
     {
-        public BasePageTest() : base()
-        {
-        }
-
         public override BrowserNewContextOptions ContextOptions()
         {
             return new BrowserNewContextOptions
@@ -22,18 +16,14 @@ namespace SAPPub.Tests.UI.Infrastructure
             };
         }
 
-        // This is called by xUnit BEFORE each test
-        public async Task InitializeAsync()
+        public override async Task InitializeAsync()
         {
-            // Ensure artifact directories exist
             Directory.CreateDirectory("SAPPub.Tests.UI/test-artifacts/screenshots");
             Directory.CreateDirectory("SAPPub.Tests.UI/test-artifacts/traces");
             Directory.CreateDirectory("SAPPub.Tests.UI/test-artifacts/videos");
 
-            // Let Playwright.PageTest initialize the browser/context/page
             await base.InitializeAsync();
 
-            // Start Playwright tracing
             await Context.Tracing.StartAsync(new()
             {
                 Screenshots = true,
@@ -42,34 +32,30 @@ namespace SAPPub.Tests.UI.Infrastructure
             });
         }
 
-        // This is called by xUnit AFTER each test
-        public async Task DisposeAsync()
+        public override async Task DisposeAsync()
         {
             string id = Guid.NewGuid().ToString();
 
-            // Stop tracing and write trace file
             await Context.Tracing.StopAsync(new()
             {
                 Path = $"SAPPub.Tests.UI/test-artifacts/traces/{id}.zip"
             });
 
-            // Save screenshot
             await Page.ScreenshotAsync(new()
             {
                 Path = $"SAPPub.Tests.UI/test-artifacts/screenshots/{id}.png",
                 FullPage = true
             });
 
-            // Dispose Playwright.PageTest resources
             await base.DisposeAsync();
         }
 
-        protected async Task<IResponse?> GoToPageAysnc(string relativeUrl)
+        protected Task<IResponse?> GoToPageAysnc(string relativeUrl)
         {
             var baseUrl = Environment.GetEnvironmentVariable("BASE_URL")
                 ?? "https://localhost:3000";
 
-            return await Page.GotoAsync($"{baseUrl}/{relativeUrl}");
+            return Page.GotoAsync($"{baseUrl}/{relativeUrl}");
         }
     }
 }
