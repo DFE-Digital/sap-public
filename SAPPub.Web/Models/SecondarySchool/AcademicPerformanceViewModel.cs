c445022f1e15f3bfe9931e07d3267dd873617f77
rgaddam-dfe <Rajesh.GADDAM@education.gov.uk>
2025-12-02 17:02:10 +0000

    Moved charts to academic performance page (#36)

--- SAPPub.Web/Models/SecondarySchool/AcademicPerformanceViewModel.cs
+++ SAPPub.Web/Models/SecondarySchool/AcademicPerformanceViewModel.cs
@@ -1,5 +1,8 @@
-﻿namespace SAPPub.Web.Models.SecondarySchool;
+﻿using SAPPub.Web.Models.Charts;
+
+namespace SAPPub.Web.Models.SecondarySchool;
 
 public class AcademicPerformanceViewModel : SecondarySchoolBaseViewModel
 {
+    public required GcseDataViewModel GcseChartData { get; set; }
 }
