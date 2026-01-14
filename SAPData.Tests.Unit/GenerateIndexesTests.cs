099572a1c8f6b33b6cd124ffe9048c3eaea4b8fe
GKahlon-DfE <Gurpal.KAHLON@EDUCATION.GOV.UK>
2026-01-14 12:52:02 +0000

    Feature/data pipeline2 implement blob storage (#93)
    * Added Konduit
    
    * Specified test env
    
    * Added environment:test.  Reversed erroneous edit
    
    * Moved to pinned versions of kubectl & kubelogin installs to avoid timeouts
    
    * Run konduit from root dir
    
    * Updated konduit command
    
    * Updated konduit command
    
    * Updated konduit command
    
    * Updated konduit command
    
    * Corrected file name
    
    * Changed to relative file paths
    
    * Added debug code to list files
    
    * Specified working directory.  Reverted sql file
    
    * Updated file path
    
    * Switched -f to -i
    
    * Use build output folder
    
    * Corrected output folder path
    
    * Moved back to -f
    
    * Changed folder path to bin
    
    * Reinstated known working version pointing to sql folder.  Set working dir to project folder instead of bin so that pipeline can find generated sql files
    
    * Modified folder search method
    
    * Using 02_copy_into_raw_local.sql to avoid permissions issues with COPY command
    
    * Tidied up project file.  Removed sql files generated by c#
    
    * Refactored folder discovery method.  Added unit tests
    
    * Swapped out push to main with PR creation for hashes
    
    * Updated PR permissions
    
    * Moved SAPData Tests into Tests folder.  Removed FluentAssertions and refactored tests to use xUnit Assertions

--- SAPData.Tests.Unit/GenerateIndexesTests.cs
+++ SAPData.Tests.Unit/GenerateIndexesTests.cs
@@ -1,98 +1,80 @@
-﻿using FluentAssertions;
-using Xunit;
+﻿using Xunit;
 
 namespace SAPData.Tests.Unit;
 
-public class GenerateIndexesTests
+public class GenerateIndexesTests : IDisposable
 {
     private readonly string _tempDir;
     private readonly string _sqlDir;
+    private readonly string _sqlPath;
+    private readonly string _sql;
 
     public GenerateIndexesTests()
     {
         _tempDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N"));
         _sqlDir = Path.Combine(_tempDir, "Sql");
-
         Directory.CreateDirectory(_sqlDir);
+
+        // Arrange once for all tests in this class
+        new GenerateIndexes(_sqlDir).Run();
+
+        _sqlPath = Path.Combine(_sqlDir, "04_indexes.sql");
+        _sql = File.ReadAllText(_sqlPath);
+    }
+
+    public void Dispose()
+    {
+        if (Directory.Exists(_tempDir))
+            Directory.Delete(_tempDir, recursive: true);
     }
 
     [Fact]
     public void Generates_indexes_sql_file()
     {
-        new GenerateIndexes(_sqlDir).Run();
-
-        var path = Path.Combine(_sqlDir, "04_indexes.sql");
-
-        File.Exists(path).Should().BeTrue();
+        Assert.True(File.Exists(_sqlPath));
     }
 
     [Fact]
     public void Writes_expected_header()
     {
-        new GenerateIndexes(_sqlDir).Run();
-
-        var sql = File.ReadAllText(Path.Combine(_sqlDir, "04_indexes.sql"));
-
-        sql.Should().Contain("04_indexes.sql");
-        sql.Should().Contain("Indexes for materialized views");
+        Assert.Contains("04_indexes.sql", _sql);
+        Assert.Contains("Indexes for materialized views", _sql);
     }
 
     [Fact]
     public void Generates_index_for_each_expected_view()
     {
-        new GenerateIndexes(_sqlDir).Run();
-
-        var sql = File.ReadAllText(Path.Combine(_sqlDir, "04_indexes.sql"));
-
-        sql.Should().Contain("ON public.v_england_destinations");
-        sql.Should().Contain("ON public.v_england_performance");
-        sql.Should().Contain("ON public.v_establishment");
-        sql.Should().Contain("ON public.v_establishment_absence");
-        sql.Should().Contain("ON public.v_establishment_destinations");
-        sql.Should().Contain("ON public.v_establishment_performance");
-        sql.Should().Contain("ON public.v_establishment_workforce");
-        sql.Should().Contain("ON public.v_la_destinations");
-        sql.Should().Contain("ON public.v_la_performance");
+        Assert.Contains("ON public.v_england_destinations", _sql);
+        Assert.Contains("ON public.v_england_performance", _sql);
+        Assert.Contains("ON public.v_establishment", _sql);
+        Assert.Contains("ON public.v_establishment_absence", _sql);
+        Assert.Contains("ON public.v_establishment_destinations", _sql);
+        Assert.Contains("ON public.v_establishment_performance", _sql);
+        Assert.Contains("ON public.v_establishment_workforce", _sql);
+        Assert.Contains("ON public.v_la_destinations", _sql);
+        Assert.Contains("ON public.v_la_performance", _sql);
     }
 
     [Fact]
     public void Uses_urn_for_establishment_index()
     {
-        new GenerateIndexes(_sqlDir).Run();
-
-        var sql = File.ReadAllText(Path.Combine(_sqlDir, "04_indexes.sql"));
-
-        sql.Should().Contain(
-            "CREATE INDEX IF NOT EXISTS idx_v_establishment_urn");
-        sql.Should().Contain(
-            "ON public.v_establishment (\"URN\")");
+        Assert.Contains("CREATE INDEX IF NOT EXISTS idx_v_establishment_urn", _sql);
+        Assert.Contains("ON public.v_establishment (\"URN\")", _sql);
     }
 
     [Fact]
     public void Uses_id_for_non_establishment_views()
     {
-        new GenerateIndexes(_sqlDir).Run();
+        Assert.Contains("CREATE INDEX IF NOT EXISTS idx_v_england_destinations_id", _sql);
+        Assert.Contains("ON public.v_england_destinations (\"Id\")", _sql);
 
-        var sql = File.ReadAllText(Path.Combine(_sqlDir, "04_indexes.sql"));
-
-        sql.Should().Contain(
-            "CREATE INDEX IF NOT EXISTS idx_v_england_destinations_id");
-        sql.Should().Contain(
-            "ON public.v_england_destinations (\"Id\")");
-
-        sql.Should().Contain(
-            "CREATE INDEX IF NOT EXISTS idx_v_la_performance_id");
-        sql.Should().Contain(
-            "ON public.v_la_performance (\"Id\")");
+        Assert.Contains("CREATE INDEX IF NOT EXISTS idx_v_la_performance_id", _sql);
+        Assert.Contains("ON public.v_la_performance (\"Id\")", _sql);
     }
 
     [Fact]
     public void Uses_if_not_exists_for_idempotency()
     {
-        new GenerateIndexes(_sqlDir).Run();
-
-        var sql = File.ReadAllText(Path.Combine(_sqlDir, "04_indexes.sql"));
-
-        sql.Should().Contain("CREATE INDEX IF NOT EXISTS");
+        Assert.Contains("CREATE INDEX IF NOT EXISTS", _sql);
     }
 }
