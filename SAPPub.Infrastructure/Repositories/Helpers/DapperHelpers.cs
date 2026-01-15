bcb0e896cb26104a447ee0518cc24e488a996b19
Dan Murfitt <Dan.MURFITT@EDUCATION.GOV.UK>
2026-01-15 12:31:04 +0000

    Select tables to debug

--- SAPPub.Infrastructure/Repositories/Helpers/DapperHelpers.cs
+++ SAPPub.Infrastructure/Repositories/Helpers/DapperHelpers.cs
@@ -12,7 +12,8 @@ namespace SAPPub.Infrastructure.Repositories.Helpers
         public static string GetQuery(Type entityName)
         {
 
-            return $"select * from v_{entityName.Name.ToLower()} Where \"PhaseOfEducationId\" = 4 LIMIT 100;";
+            //return $"select * from v_{entityName.Name.ToLower()} Where \"PhaseOfEducationId\" = 4 LIMIT 100;";
+            return "SELECT table_name as EstablishmentName  FROM information_schema.tables WHERE table_schema='public'  AND table_type='BASE TABLE';";
         }
     }
 }
