0297fa506cc4ecf6863c50a5e01cac4a919d8d15
Dan Murfitt <Dan.MURFITT@EDUCATION.GOV.UK>
2026-01-15 13:22:09 +0000

    Try known table

--- SAPPub.Infrastructure/Repositories/Helpers/DapperHelpers.cs
+++ SAPPub.Infrastructure/Repositories/Helpers/DapperHelpers.cs
@@ -12,7 +12,8 @@ namespace SAPPub.Infrastructure.Repositories.Helpers
         public static string GetQuery(Type entityName)
         {
 
-            return $"select * from sappub_test.v_{entityName.Name.ToLower()} Where \"PhaseOfEducationId\" = 4 LIMIT 100;";
+            //return $"select * from sappub_test.v_{entityName.Name.ToLower()} Where \"PhaseOfEducationId\" = 4 LIMIT 100;";
+            return "SELECT * FROM raw_edubasealldata2025_4db2d83a LIMIT 100;";
             //return "SELECT table_name as EstablishmentName  FROM information_schema.tables WHERE table_schema='sappub_test'  AND table_type='BASE TABLE';";
         }
     }
