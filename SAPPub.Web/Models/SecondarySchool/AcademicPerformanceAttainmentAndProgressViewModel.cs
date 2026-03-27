574e81a8d0ae1d3789bd004ab920fc3ead908bd4
Catherine Lawlor <55924986+CathLass@users.noreply.github.com>
2026-03-27 12:50:10 +0000

    Add Attainment 8 context text to the school’s Attainment 8 score. (#261)
    # Description
    Add Attainment 8 context text to the school’s Attainment 8 score.
    After the numeric school score, display a new sentence:
    
    “This means that pupils generally scored the equivalent of [grade
    descriptor] in their 8 best GCSE-level subjects.”
    
    https://trello.com/c/AR8k3tCp
    
    ## Type of change
    
    Please delete options that are not relevant.
    - [ ] New feature (non-breaking change which adds functionality)

--- SAPPub.Web/Models/SecondarySchool/AcademicPerformanceAttainmentAndProgressViewModel.cs
+++ SAPPub.Web/Models/SecondarySchool/AcademicPerformanceAttainmentAndProgressViewModel.cs
@@ -21,6 +21,7 @@ public class AcademicPerformanceAttainmentAndProgressViewModel : SecondarySchool
     public double? LocalAuthorityProgress8Score { get; init; }
 
     public double? EstablishmentAttainment8Score { get; init; }
+    public required DisplayField<string> EstablishmentAttainment8ScoreContextDescription { get; init; }
 
     public double? LocalAuthorityAttainment8Score { get; init; }
 
@@ -38,6 +39,7 @@ public class AcademicPerformanceAttainmentAndProgressViewModel : SecondarySchool
 
     public static AcademicPerformanceAttainmentAndProgressViewModel Map(AttainmentAndProgressModel attainmentAndProgressModel, AcademicYearSelection selectedAcademicYear)
     {
+        var establishmentAttainment8Context = EstablishmentAttainment8ContextStatement(attainmentAndProgressModel.EstablishmentAttainment8Score);
         return new AcademicPerformanceAttainmentAndProgressViewModel
         {
             URN = attainmentAndProgressModel.Urn,
@@ -46,10 +48,39 @@ public class AcademicPerformanceAttainmentAndProgressViewModel : SecondarySchool
             EstablishmentProgress8Score = attainmentAndProgressModel.EstablishmentProgress8Score,
             LocalAuthorityProgress8Score = attainmentAndProgressModel.LocalAuthorityProgress8Score,
             EstablishmentAttainment8Score = attainmentAndProgressModel.EstablishmentAttainment8Score,
+            EstablishmentAttainment8ScoreContextDescription = establishmentAttainment8Context != null
+                ? ($"This means that pupils generally scored the equivalent of {establishmentAttainment8Context} in their 8 best GCSE-level subjects.").ToDisplayField()
+                : DisplayField<string>.NotAvailable(),
             LocalAuthorityAttainment8Score = attainmentAndProgressModel.LocalAuthorityAttainment8Score,
             EnglandAttainment8Score = attainmentAndProgressModel.EnglandAttainment8Score,
             EstablishmentProgress8TotalPupils = attainmentAndProgressModel.EstablishmentProgress8TotalPupils,
             EstablishmentTotalPupils = attainmentAndProgressModel.EstablishmentTotalPupils
         };
+    }
+
+    private static string? EstablishmentAttainment8ContextStatement(double? score)
+    {
+        if (!score.HasValue || score.Value < 0 || score.Value > 90)
+            return null;
+
+        if (score == 90)
+            return "grade 9";
+
+        if (score < 10)
+            return "below a grade 1";
+
+        int baseGrade = (int)(score.Value / 10);   // 10–19 → 1, 20–29 → 2, etc.
+        float remainder = (float)Math.Round(score.Value % 10, 1);
+
+        if (remainder <= 0.9f)
+            return $"grade {baseGrade}";
+
+        if (remainder <= 2.9f)
+            return $"just above grade {baseGrade}";
+
+        if (remainder <= 8.0f)
+            return $"between grade {baseGrade} and grade {baseGrade + 1}";
+
+        return $"just below grade {baseGrade + 1}";
     }
 }
