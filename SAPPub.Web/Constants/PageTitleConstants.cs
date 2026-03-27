05318a56027d066d8e04bcaeacb57d985f9ee5f6
Naomi Todd <Naomi.TODD@education.gov.uk>
2026-03-27 16:21:41 +0000

    update to file-scoped namespace for constants and made SAPPub.Web.Constants a global using

--- SAPPub.Web/Constants/PageTitleConstants.cs
+++ SAPPub.Web/Constants/PageTitleConstants.cs
@@ -1,12 +1,12 @@
-﻿namespace SAPPub.Web.Constants
+﻿namespace SAPPub.Web.Constants;
+
+public class PageTitleConstants
 {
-    public class PageTitleConstants
+    public static class SecondarySchoolPageTitles
     {
-        public static class SecondarySchoolPageTitles
-        {
-            public const string EnglishAndMathsResults = "English and maths results";
-            public const string AttainmentAndProgress = "Attainment and progress";
-            public const string SubjectsEntered = "Subjects entered";
-        }
+        public const string EnglishAndMathsResults = "English and maths results";
+        public const string AttainmentAndProgress = "Attainment and progress";
+        public const string SubjectsEntered = "Subjects entered";
     }
 }
+
