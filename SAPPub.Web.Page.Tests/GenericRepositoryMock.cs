f7d18a398597c1621709da5273882025f72efb7b
CLAWLOR <catherine.lawlor@education.gov.uk>
2026-04-13 10:52:33 +0100

    delete unneeded files

--- SAPPub.Web.Page.Tests/GenericRepositoryMock.cs
+++
@@ -1,8 +0,0 @@
-﻿using Moq;
-using SAPPub.Core.Interfaces.Repositories.Generic;
-
-namespace SAPPub.Web.Page.Tests;
-
-public class GenericRepositoryMock<T> : Mock<IGenericRepository<T>> where T : class
-{
-}
