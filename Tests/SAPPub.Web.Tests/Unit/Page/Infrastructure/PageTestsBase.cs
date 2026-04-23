4432cadc0ee5e00262843b1a0be9a99687129a76
CLAWLOR <catherine.lawlor@education.gov.uk>
2026-04-23 17:07:25 +0100

    remove unwanted

--- Tests/SAPPub.Web.Tests/Unit/Page/Infrastructure/PageTestsBase.cs
+++ Tests/SAPPub.Web.Tests/Unit/Page/Infrastructure/PageTestsBase.cs
@@ -4,8 +4,6 @@ namespace SAPPub.Web.Tests.Unit.Page.Infrastructure;
 
 public class PageTestsBase
 {
-    //private readonly MockAccessor _accessor;
-
     protected string BuildUrl(string urn, string schoolName, string pageRoute)
     {
         var encodedSchoolName = TextHelpers.CleanForUrl(schoolName);
