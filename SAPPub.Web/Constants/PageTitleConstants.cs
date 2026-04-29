2abdafb7a75cad755f095fdb9da285e905b09755
GADDAM, Rajesh <Rajesh.GADDAM@EDUCATION.GOV.UK>
2026-04-29 16:03:12 +0100

    Rename Attainment and Progress page to Progress and attainment - Renamed page title, heading and pagination - Fixed issue with pagination - Added unit tests

--- SAPPub.Web/Constants/PageTitleConstants.cs
+++ SAPPub.Web/Constants/PageTitleConstants.cs
@@ -5,7 +5,7 @@ public class PageTitleConstants
     public static class SecondarySchoolPageTitles
     {
         public const string EnglishAndMathsResults = "English and maths results";
-        public const string AttainmentAndProgress = "Attainment and progress";
+        public const string ProgressAndAttainment = "Progress and attainment";
         public const string SubjectsEntered = "Subjects entered";
     }
 }
