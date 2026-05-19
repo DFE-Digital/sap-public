01176db0e5c05e54bf1651dceb8867c186a83bd5
Naomi Todd <Naomi.TODD@education.gov.uk>
2026-05-19 14:23:17 +0100

    Add start of secondary filter update (still need to cross reference performance urns)

--- SAPData/Filters/SqlViewFilterProvider.cs
+++ SAPData/Filters/SqlViewFilterProvider.cs
@@ -6,12 +6,19 @@
         {
             return
             [
-                new SqlViewFilter("ExcludeOnlineSchools", tableAlias =>
-                    $"clean_int({tableAlias}.\"typeofestablishment__code_\") <> 49"),
+                new SqlViewFilter("ExcludeNurseries", tableAlias =>
+                    $"clean_int({tableAlias}.\"phaseofeducation__code_\") <> 1"),
+                new SqlViewFilter("IncludeOnlyInScopeSchoolTypes", tableAlias =>
+                    $"clean_int({tableAlias}.\"typeofestablishment__code_\") IN (1, 2, 3, 5, 6, 7, 8, 10, 11, 12, 18, 26, 28, 31, 33, 34, 35, 36, 39, 40, 41, 44, 45, 46, 56)"),
                 new SqlViewFilter("ExcludeClosed3YrSchools", tableAlias =>
                     $"({tableAlias}.\"closedate\" IS NULL OR {tableAlias}.\"closedate\" = '' OR TO_DATE({tableAlias}.\"closedate\", 'DD/MM/YYYY') >= '{GetAcademicYearCutoffDate()}')"),
                 new SqlViewFilter("IncludeKS4", tableAlias =>
-                    $"(clean_int({tableAlias}.\"phaseofeducation__code_\") IN (4, 5, 7) OR (clean_int({tableAlias}.\"phaseofeducation__code_\") = 0 AND clean_int({tableAlias}.\"typeofestablishment__code_\") = 06))"),
+                    $"(clean_int({tableAlias}.\"phaseofeducation__code_\") IN (0, 3, 4, 5, 6, 7) " +
+                    $"AND clean_int({tableAlias}.\"statutorylowage\") < 16 " +
+                    $"AND clean_int({tableAlias}.\"statutoryhighage\") > 12) " +
+                    $"OR (clean_int({tableAlias}.\"phaseofeducation__code_\") IN (4, 7) " +
+                    $"AND clean_int({tableAlias}.\"statutoryhighage\") = 0)"
+                ),
                 new SqlViewFilter("ExcludeProposedToOpen", tableAlias =>
                     $"clean_int({tableAlias}.\"establishmentstatus__code_\") <> 4")
             ];
