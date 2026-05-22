390db51257eece28892df977edc113b78240876f
Naomi Todd <Naomi.TODD@education.gov.uk>
2026-05-22 13:59:50 +0100

    Remove no longer neede mapping methods

--- SAPPub.Core/Helpers/MappingHelper.cs
+++ SAPPub.Core/Helpers/MappingHelper.cs
@@ -36,28 +36,6 @@ public class MappingHelper
         return GeoUK.Convert.ToLatitudeLongitude(new Wgs84(), wgsCartesian);
     }
 
-    public static double? HaversineMiles(double lat1, double lon1, double? lat2, double? lon2)
-    {
-        if (lat2 is null || lon2 is null)
-        {
-            return null;
-        }
-        double ToRad(double d) => Math.PI * d / 180.0;
-        const double Rmiles = 3958.7613;
-        var dLat = ToRad(lat2.Value - lat1);
-        var dLon = ToRad(lon2.Value - lon1);
-        var a = Math.Pow(Math.Sin(dLat / 2), 2) +
-                Math.Cos(ToRad(lat1)) * Math.Cos(ToRad(lat2.Value)) * Math.Pow(Math.Sin(dLon / 2), 2);
-        var c = 2 * Math.Asin(Math.Sqrt(a));
-        return Rmiles * c;
-    }
-
-    public static double MilesToDegrees(double miles)
-    {
-        const double metersPerMile = 1609.344;
-        const double metersPerDegree = 111_320.0;   // 1 degree latitude in meters
-        return miles * metersPerMile / metersPerDegree;
-    }
     public static double MilesToMeters(double miles)
     {
         const double metersPerMile = 1609.344;
