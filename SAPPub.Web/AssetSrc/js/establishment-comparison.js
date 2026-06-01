e4bb9d52a40ee6147941bd0800c69a89ebd05ce0
stuw-dfe <281972301+stuw-dfe@users.noreply.github.com>
2026-06-01 12:09:49 +0100

    Added javascript version of establishment comparison add/remove

---
+++ SAPPub.Web/AssetSrc/js/establishment-comparison.js
@@ -0,0 +1,49 @@
+﻿(function () {
+    document.addEventListener('submit', async function (e) {
+        const form = e.target;
+        const establishmentComparisonConfig = window.establishmentComparisonConfig;
+
+        if (!form.classList.contains("save-establishment-comparison-form")) {
+            return;
+        }
+
+        e.preventDefault();
+
+        const response = await fetch(form.action, {
+            method: "POST",
+            body: new FormData(form),
+            headers: { "X-Requested-With": "XMLHttpRequest" }
+        });
+
+        const data = await response.json();
+        const button = form.querySelector("button");
+        const addSuccessBanner = document.getElementById("comparison-add-success");
+        const removeSuccessBanner = document.getElementById("comparison-remove-success");
+        resetNotificationBanner(addSuccessBanner);
+        resetNotificationBanner(removeSuccessBanner);
+
+        button.classList.toggle("saved", data.saved);
+        if (data.saved) {
+            button.classList.add("saved");
+            button.innerHTML = `<span>${establishmentComparisonConfig.savedText}</span>`;
+            showNotificationBanner(addSuccessBanner);
+        } else {
+            button.classList.remove("saved");
+            button.innerHTML = `<span>${establishmentComparisonConfig.saveText}</span>`;
+            const limitNotificationBanner = document.getElementById("school-compare-limit-notification");
+            if (limitNotificationBanner !== null) {
+                limitNotificationBanner.classList.add("govuk-visually-hidden");
+            }
+            showNotificationBanner(removeSuccessBanner);
+        }
+    });
+
+    function resetNotificationBanner(bannerElement) {
+        bannerElement.classList.add("govuk-visually-hidden");
+    }
+
+    function showNotificationBanner(bannerElement) {
+        bannerElement.classList.remove("govuk-visually-hidden");
+    }
+
+})();
