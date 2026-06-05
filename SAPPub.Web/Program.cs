141049f5099bb7d98569903e8fb60bf70fc8b8fb
CLAWLOR <catherine.lawlor@education.gov.uk>
2026-06-05 12:48:48 +0100

    remove unneeded

--- SAPPub.Web/Program.cs
+++ SAPPub.Web/Program.cs
@@ -9,7 +9,6 @@ using Notify.Client;
 using Notify.Interfaces;
 using Npgsql;
 using SAPPub.Core.Interfaces.Services;
-using SAPPub.Core.Services;
 using SAPPub.Infrastructure.PostcodeLookup;
 using SAPPub.Web.Helpers;
 using SAPPub.Web.Middleware;
@@ -118,9 +117,6 @@ public partial class Program
         builder.Services.AddScoped<INotificationClient>((sp) => new NotificationClient(emailAPIKey));
 
         builder.Services.AddDependencies(builder.Environment, builder.Configuration);
-
-        // CML TODO temp
-        builder.Services.AddScoped<IEstablishmentComparisonService, EstablishmentComparisonService>();
 
         // Add custom error handler for NotFoundExceptions
         builder.Services.AddExceptionHandler<NotFoundExceptionHandler>();
