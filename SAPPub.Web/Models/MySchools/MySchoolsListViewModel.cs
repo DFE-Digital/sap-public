85f85af6bbacf84c9ce246ac458ede810c8ed77e
CLAWLOR <catherine.lawlor@education.gov.uk>
2026-06-05 12:46:22 +0100

    remove comment

--- SAPPub.Web/Models/MySchools/MySchoolsListViewModel.cs
+++ SAPPub.Web/Models/MySchools/MySchoolsListViewModel.cs
@@ -3,5 +3,4 @@
 public class MySchoolsListViewModel
 {
     public IReadOnlyCollection<MySchoolModel> MySchools { get; set; } = Array.Empty<MySchoolModel>();
-    //public List<string>? SelectedSchools { get; set; }
 }
