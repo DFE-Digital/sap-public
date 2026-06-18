136d93d53202812e53cab5e8ef5080dd7fdd3a04
stuw-dfe <281972301+stuw-dfe@users.noreply.github.com>
2026-06-18 17:03:47 +0100

    Education Phase information added to the school profile about page

---
+++ SAPPub.Core/Extensions/EducationPhaseFormatter.cs
@@ -0,0 +1,32 @@
+﻿namespace SAPPub.Core.Extensions;
+
+public static class EducationPhaseFormatter
+{
+    public static string? Format(bool isKS2, bool isKS4, bool isKS5)
+    { 
+        var phaseList = new List<string>();
+
+        if (isKS2)
+        {
+            phaseList.Add("Primary");
+        }
+
+        if (isKS4)
+        {
+            phaseList.Add("Secondary");
+        }
+
+        if (isKS5)
+        {
+            phaseList.Add("Post-16");
+        }
+
+        return phaseList.Count switch
+        {
+            0 => null,
+            1 => phaseList[0],
+            2 => $"{phaseList[0]} and {phaseList[1]}",
+            _ => $"{phaseList[0]}, {phaseList[1]} and {phaseList[2]}"
+        };
+    }
+}
---
+++ Tests/SAPPub.Core.Tests/Helpers/EducationalPhaseFormatterTests.cs
@@ -0,0 +1,26 @@
+﻿using SAPPub.Core.Extensions;
+
+namespace SAPPub.Core.Tests.Helpers
+{
+    public class EducationalPhaseFormatterTests
+    {
+        [Theory]
+        [InlineData(false, false, false, null)]
+        [InlineData(true, false, false, "Primary")]
+        [InlineData(true, true, false, "Primary and Secondary")]
+        [InlineData(true, true, true, "Primary, Secondary and Post-16")]
+        [InlineData(false, true, true, "Secondary and Post-16")]
+        [InlineData(false, false, true, "Post-16")]
+        [InlineData(false, true, false, "Secondary")]
+        [InlineData(true, false, true, "Primary and Post-16")]
+        public void CleanForUrl_RemovesNonAlphaChars(bool isKS2, bool isKS4, bool isKS5, string? output)
+        {
+            // Arrange
+            // Act
+            var result = EducationPhaseFormatter.Format(isKS2, isKS4, isKS5);
+
+            // Assert
+            Assert.Equal(output, result);
+        }
+    }
+}
