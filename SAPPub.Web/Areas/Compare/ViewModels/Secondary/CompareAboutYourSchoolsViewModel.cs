833fd1faf03c7db67e7d21a30c69b7781683d4f8
stuw-dfe <281972301+stuw-dfe@users.noreply.github.com>
2026-06-29 09:15:12 +0100

    Small tweaks/tidy ups

--- SAPPub.Web/Areas/Compare/ViewModels/Secondary/CompareAboutYourSchoolsViewModel.cs
+++ SAPPub.Web/Areas/Compare/ViewModels/Secondary/CompareAboutYourSchoolsViewModel.cs
@@ -11,27 +11,6 @@ public class CompareAboutYourSchoolsViewModel : CompareSecondarySchoolBaseViewMo
 
     public IEnumerable<MapData> MapData { get; set; } = [];
 
-    public class CompareAboutYourSchoolViewModel
-    {
-        public required string Urn { get; set; }
-
-        public required string SchoolName { get; set; }
-
-        public string? Website { get; set; }
-
-        public DisplayField<string> Address { get; set; }
-
-        public string? LocalAuthority { get; set; }
-
-        public string? LocalAuthorityName { get; set; }
-
-        public string? LocalAuthorityWebsite { get; set; }
-
-        public double? Latitude { get; set; }
-
-        public double? Longitude { get; set; }
-    }
-
 
     public static CompareAboutYourSchoolsViewModel Map(
         List<string> urns,
