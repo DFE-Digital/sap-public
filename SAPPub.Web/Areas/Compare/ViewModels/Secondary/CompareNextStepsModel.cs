036067f50145f1643ee5f737922bc3472b5c25da
stuw-dfe <281972301+stuw-dfe@users.noreply.github.com>
2026-06-30 09:37:25 +0100

    Next steps comparison content and school info added

---
+++ SAPPub.Web/Areas/Compare/ViewModels/Secondary/CompareNextStepsModel.cs
@@ -0,0 +1,17 @@
+﻿using SAPPub.Core.Helpers;
+using SAPPub.Web.Helpers;
+
+namespace SAPPub.Web.Areas.Compare.ViewModels.Secondary;
+
+public class CompareNextStepsModel
+{
+    public string? EstablishmentName { get; set; }
+
+    public string EstablishmentNameClean => TextHelpers.CleanForUrl(EstablishmentName!);
+
+    public string? URN { get; set; }
+
+    public required DisplayField<string> Website { get; set; }
+
+    public required DisplayField<string> Telephone { get; set; }
+}
