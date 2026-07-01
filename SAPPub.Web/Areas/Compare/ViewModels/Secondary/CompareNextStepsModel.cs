6e958c983931d6deece53edb3fbc25e4a1c61308
stuw-dfe <281972301+stuw-dfe@users.noreply.github.com>
2026-07-01 12:14:21 +0100

    Fixed and improved tests for next steps comparison page

--- SAPPub.Web/Areas/Compare/ViewModels/Secondary/CompareNextStepsModel.cs
+++ SAPPub.Web/Areas/Compare/ViewModels/Secondary/CompareNextStepsModel.cs
@@ -1,13 +1,10 @@
-﻿using SAPPub.Core.Helpers;
-using SAPPub.Web.Helpers;
+﻿using SAPPub.Web.Helpers;
 
 namespace SAPPub.Web.Areas.Compare.ViewModels.Secondary;
 
 public class CompareNextStepsModel
 {
     public string? EstablishmentName { get; set; }
-
-    public string EstablishmentNameClean => TextHelpers.CleanForUrl(EstablishmentName!);
 
     public string? URN { get; set; }
 
--- Tests/SAPPub.Web.Tests/Unit/Page/Areas/Compare/Secondary/NextStepsPageTests.cs
+++ Tests/SAPPub.Web.Tests/Unit/Page/Areas/Compare/Secondary/NextStepsPageTests.cs
@@ -12,6 +12,11 @@ public class NextStepsPageTests : PageTestsBase
     private string _pageUrl = "compare/secondary/next-steps?urns=119052&urns=124500";
     private readonly Mock<IEstablishmentService> _establishmentService;
 
+    private string schoolName1 = "xyz School";
+    private string schoolName2 = "abc School";
+    private string website1 = "https://www.test.com";
+    private string telephone1 = "101011";
+
     public NextStepsPageTests(WebAppFixture fixture) : base(fixture)
     {
         // set up the mock establishment service to return establishments for the URNs in the query string
@@ -19,13 +24,18 @@ public class NextStepsPageTests : PageTestsBase
         _establishmentService = UseMock<IEstablishmentService>();
         var establishmentList = (new List<Establishment>
         {
-            new EstablishmentTestBuilder().WithURN("119052").WithEstablishmentName("xyz School").WithIsKeyStage4(true).Build(),
-            new EstablishmentTestBuilder().WithURN("124500").WithEstablishmentName("abc School").WithIsKeyStage4(true).Build(),
+            new EstablishmentTestBuilder().WithURN("119052").WithEstablishmentName(schoolName1).WithTelephoneNum(telephone1).WithWebsite(website1).WithIsKeyStage4(true).Build(),
+            new EstablishmentTestBuilder().WithURN("124500").WithEstablishmentName(schoolName2).WithIsKeyStage4(true).Build(),
         }).ToList();
+
 
         establishmentList.Select(e =>
             _establishmentService.Setup(s =>
             s.GetEstablishmentAsync(e.URN, It.IsAny<CancellationToken>())).ReturnsAsync(e)).ToList();
+
+        _establishmentService
+            .Setup(s => s.GetEstablishmentsAsync(It.IsAny<List<string>>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(establishmentList);
     }
 
     [Fact]
@@ -81,8 +91,18 @@ public class NextStepsPageTests : PageTestsBase
         // Act
         var doc = await Fixture.BrowseToPage(_pageUrl);
         var summaryLists = doc.GetElementsByClassName("govuk-summary-list");
-        
+
         // Assert
-        Assert.NotNull(summaryLists);
+        Assert.Equal(2, summaryLists.Count());
+        var summaryListOneValues = summaryLists[0].QuerySelectorAll(".govuk-summary-list__value");
+        var summaryListTwoValues = summaryLists[1].QuerySelectorAll(".govuk-summary-list__value");
+
+        Assert.Contains($"View {schoolName2}'s Profile", summaryListOneValues[0].TextContent);
+        Assert.Contains($"Not available", summaryListOneValues[1].TextContent.Trim());
+        Assert.Contains($"Not available", summaryListOneValues[2].TextContent.Trim());
+
+        Assert.Contains($"View {schoolName1}'s Profile", summaryListTwoValues[0].TextContent);
+        Assert.Contains($"https://www.test.com", summaryListTwoValues[1].TextContent);
+        Assert.Contains($"101011", summaryListTwoValues[2].TextContent.Trim());
     }
 }
