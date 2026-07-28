6f2859cbdea7460388e5dcb0a3e62a218386a092
CLAWLOR <catherine.lawlor@education.gov.uk>
2026-07-28 17:14:48 +0100

    test fixes

--- Tests/SAPPub.Web.Tests/UI/AccessibilityTests.cs
+++ Tests/SAPPub.Web.Tests/UI/AccessibilityTests.cs
@@ -11,11 +11,20 @@ public class AccessibilityTests(WebApplicationSetupFixture fixture) : BasePageTe
 {
     private List<string> _pageUrls = new List<string>
     {
+        // common pages
         "school/105574/loreto-high-school-chorlton/about",
         "school/137552/stewards-academy-science-specialist-harlow/about",
         "school/100273/saint-paul-roman-catholic-infant-school/about",
         "school/107564/todmorden-high-school/about",
         "school/145744/abbey-park-school/about",
+
+        // primary pages
+        "school/100273/saint-paul-roman-catholic-infant-school/primary-performance/pupil-progress",
+        "school/100273/saint-paul-roman-catholic-infant-school/primary-performance/additional-measures",
+        "school/100273/saint-paul-roman-catholic-infant-school/admissions/primary",
+        "school/100273/saint-paul-roman-catholic-infant-school/curriculum/primary",
+
+        // secondary pages
         "school/105574/loreto-high-school-chorlton/secondary-performance/progress-attainment",
         "school/105574/loreto-high-school-chorlton/secondary-performance/progress-attainment/current",
         "school/105574/loreto-high-school-chorlton/secondary-performance/english-and-maths",
@@ -31,14 +40,24 @@ public class AccessibilityTests(WebApplicationSetupFixture fixture) : BasePageTe
         "school/100273/saint-paul-roman-catholic-infant-school/curriculum/secondary",
         "school/105574/loreto-high-school-chorlton/destinations/secondary",
         "school/100273/saint-paul-roman-catholic-infant-school/destinations/secondary",
+
+        // 16-19 pages
         "school/105574/loreto-high-school-chorlton/destinations/16-to-19",
+
+        // search pages
         "search",
         "search/results?NameSearchTerm=school&Distance=3&PageNumber=1",
         "search/results?NameSearchTerm=xyz&Distance=3&PageNumber=1",
+
+        // miscellaneous pages
         "",
         "Cookies/Preferences",
+
+        // my schools pages
         "my-schools/view",
         "my-schools/no-schools-added",
+
+        // compare secondary pages
         "compare/secondary/about-your-schools?urns=105574&urns=107564",
         "compare/secondary/pupil-attainment?urns=100279&urns=145179",
         "compare/secondary/english-and-maths-results?urns=105574&urns=137020",
