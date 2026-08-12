6c165e4f774a078860c7520dd10a74970d546493
Naomi Todd <Naomi.TODD@education.gov.uk>
2026-08-12 16:52:04 +0100

    Add 'not available' page tests for pupil population

--- Tests/SAPPub.Web.Tests/Unit/Page/Areas/Profiles/KS2AdditionalMeasuresPageTests.cs
+++ Tests/SAPPub.Web.Tests/Unit/Page/Areas/Profiles/KS2AdditionalMeasuresPageTests.cs
@@ -8,6 +8,7 @@ using SAPPub.Core.Tests.TestBuilders;
 using SAPPub.Core.ValueObjects;
 using SAPPub.Web.Tests.Unit.Page.Infrastructure;
 
+
 namespace SAPPub.Web.Tests.Unit.Page.Areas.Profiles;
 
 [Collection("WebAppCollection")]
@@ -168,7 +169,41 @@ public class KS2AdditionalMeasuresPageTests : PageTestsBase
         Assert.Equal(expectedModel.EstablishmentSENSupportPopulation!.Value.ToString() + "%", doc.GetTableCellContentByIdAndIndex("sen-population-table", 1, 0));
         Assert.Equal(expectedModel.EnglandSENSupportPopulation!.Value.ToString() + "%", doc.GetTableCellContentByIdAndIndex("sen-population-table", 1, 1));
 
+    }
 
+
+    [Fact]
+    public async Task AdditionalMeasures_PupilPopulationAccordion_DataNotAvailable_ShowsTableValuesAsNotAvailable()
+    {
+        // Arrange
+        var modelWithNoData = new KS2AdditionalMeasuresModel
+        {
+            EnglandGrammarAtExpectedStandard = new CodedDouble(1, string.Empty, "1"),
+            EnglandGrammarAtHigherStandard = new CodedDouble(1, string.Empty, "2"),
+            EstablishmentGrammarAtExpectedStandard = new CodedDouble(1, string.Empty, "3"),
+            EstablishmentGrammarAtHigherStandard = new CodedDouble(1, string.Empty, "4"),
+            LAGrammarAtExpectedStandard = new CodedDouble(1, string.Empty, "7"),
+            LAGrammarAtHigherStandard = new CodedDouble(1, string.Empty, "8"),
+            EstablishmentEHCPPopulation = CodedDouble.Empty,
+            EnglandEHCPPopulation = new CodedDouble(null, "Redacted for confidentiality", "c"),
+            EstablishmentSENSupportPopulation = new CodedDouble(null, "Not applicable", "z"),      
+            EnglandSENSupportPopulation = new CodedDouble(null, "Not available", "x")
+        };
+
+        _ks2AdditionalMeasuresService
+            .Setup(s => s.GetAdditionalMeasures(_urn, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(modelWithNoData);
+
+        var url = BuildUrl(_establishment.URN, _establishment.EstablishmentName, _pageRoute);
+
+        // Act
+        var doc = await Fixture.BrowseToPage(url);
+
+        // Assert
+        Assert.Contains("Not available", doc.GetTableCellContentByIdAndIndex("ehcp-population-table", 1, 0));
+        Assert.Contains("Not available", doc.GetTableCellContentByIdAndIndex("ehcp-population-table", 1, 1));
+        Assert.Contains("Not available", doc.GetTableCellContentByIdAndIndex("sen-population-table", 1, 0));
+        Assert.Contains("Not available", doc.GetTableCellContentByIdAndIndex("sen-population-table", 1, 1));
     }
 
     private static KS2AdditionalMeasuresModel GetKS2AdditionalMeasuresModel()
