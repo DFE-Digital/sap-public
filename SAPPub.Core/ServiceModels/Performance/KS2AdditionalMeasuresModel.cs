91a68479fd80b2b07bbd0cc810d9fc67efcf33cc
Naomi Todd <99014759+naomitodd@users.noreply.github.com>
2026-08-13 17:13:46 +0100

    Feature/859 pupil population additional measures (#471)
    # Description
    
    Add pupil population EHCP/SEN figures to ks2 additional measures page.
    
    
    https://trello.com/c/8EtVlsoS/859-8b-add-pupil-population-to-the-academic-performance-additional-measures-page
    
    ## Type of change
    
    - [x] New feature (non-breaking change which adds functionality)
    
    # Checklist:
    
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my code
    - [x] My changes generate no new warnings
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit tests pass locally with my changes

--- SAPPub.Core/ServiceModels/Performance/KS2AdditionalMeasuresModel.cs
+++ SAPPub.Core/ServiceModels/Performance/KS2AdditionalMeasuresModel.cs
@@ -6,8 +6,13 @@ public class KS2AdditionalMeasuresModel
 {
     public required CodedDouble EstablishmentGrammarAtExpectedStandard { get; init; }
     public required CodedDouble EstablishmentGrammarAtHigherStandard { get; init; }
+    public required CodedDouble EstablishmentEHCPPopulation { get; init; }
+    public required CodedDouble EstablishmentSENSupportPopulation { get; init; }
+
     public required CodedDouble LAGrammarAtExpectedStandard { get; init; }
     public required CodedDouble LAGrammarAtHigherStandard { get; init; }
     public required CodedDouble EnglandGrammarAtExpectedStandard { get; init; }
     public required CodedDouble EnglandGrammarAtHigherStandard { get; init; }
+    public required CodedDouble EnglandEHCPPopulation { get; init; }
+    public required CodedDouble EnglandSENSupportPopulation { get; init; }
 }
