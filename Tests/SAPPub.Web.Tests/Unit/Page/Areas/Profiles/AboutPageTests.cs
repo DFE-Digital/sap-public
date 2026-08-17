a93560493d376626b16e4bfcdedd6c2182f33a79
Naomi Todd <Naomi.TODD@education.gov.uk>
2026-08-17 16:27:41 +0100

    add page tests for feature flag - default to true to not break existing tests, only set if needed

--- Tests/SAPPub.Web.Tests/Unit/Page/Areas/Profiles/AboutPageTests.cs
+++ Tests/SAPPub.Web.Tests/Unit/Page/Areas/Profiles/AboutPageTests.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using Microsoft.FeatureManagement;
+using Moq;
 using SAPPub.Core.Enums;
 using SAPPub.Core.Extensions;
 using SAPPub.Core.Interfaces.Services;
@@ -472,5 +473,31 @@ public class AboutPageTests : PageTestsBase
         Assert.Contains(notAvailableContent, doc.GetRowContentByIdAndKey("primary-school-features-summary", "Nursery"));
         Assert.Contains(notAvailableContent, doc.GetRowContentByIdAndKey("primary-school-features-summary", "Free breakfast club programme"));
         Assert.Contains(notRecordedContent, doc.GetRowContentByIdAndKey("primary-school-features-summary", "Wraparound care"));
+    }
+
+    [Fact]
+    public async Task AboutPage_HidesPrimarySchoolFeatures_WhenFeatureFlagDisabled()
+    {
+        // Arrange
+        var featureManagerMock = UseMock<IFeatureManager>();
+        featureManagerMock
+            .Setup(f => f.IsEnabledAsync(Constants.Constants.EnablePrimary))
+            .ReturnsAsync(false);
+
+        var aboutSchoolModel = new AboutSchoolModel
+        {
+            Urn = "143034",
+            SchoolName = "St David's Church of England Academy",
+            IsKS2 = true
+        };
+        _about
+            .Setup(service => service.GetAboutSchoolDetailsAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(aboutSchoolModel);
+
+        // Act
+        var doc = await Fixture.BrowseToPage(BuildUrl(aboutSchoolModel.Urn, aboutSchoolModel.SchoolName, _pageRoute));
+
+        // Assert
+        Assert.Null(doc.GetElementById("primary-school-features-summary"));
     }
 }
