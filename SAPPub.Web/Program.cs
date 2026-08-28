925c021e74ac70350658bed4ed67e6669cb431a5
SINGH-GILL, Manraj <Manraj.SINGH-GILL@EDUCATION.GOV.UK>
2026-08-28 11:45:56 +0100

    Upgraded code to .NET 10 and also updated all packages to use latest version

--- SAPPub.Web/Program.cs
+++ SAPPub.Web/Program.cs
@@ -49,6 +49,7 @@ public partial class Program
         builder.Services.AddHttpClient<IPostcodeLookupService, PostcodeLookupService>();
         builder.Services.AddGovUkFrontend(options =>
         {
+            //options.Rebrand = true;
             options.GetCspNonceForRequest = context =>
             {
                 return CSPHelper.RandomCharacters;
