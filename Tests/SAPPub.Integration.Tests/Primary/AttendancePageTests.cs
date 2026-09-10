b3bcec559e1b4d52ba8a09e9d486b27d8a61d408
CLAWLOR <catherine.lawlor@education.gov.uk>
2026-09-10 20:25:42 +0100

    remove unused data from test

--- Tests/SAPPub.Integration.Tests/Primary/AttendancePageTests.cs
+++ Tests/SAPPub.Integration.Tests/Primary/AttendancePageTests.cs
@@ -8,18 +8,16 @@ public class AttendancePageTests : BasePageTest
     private string pageUnderTest => "attendance";
 
     [Theory]
-    [InlineData("100019", 4.28844, "284", "23", 8.09859)]
-    [InlineData("100241", 4.51361, "359", "36", 10.02786)]
-    [InlineData("100353", 3.9508, "360", "18", 5.0)]
-    [InlineData("100448", 6.1856, "177", "36", 20.33898)]
-    [InlineData("100500", 6.44356, "166", "33", 19.87952)]
-    [InlineData("100674", 5.16147, "355", "60", 16.90141)]
-    [InlineData("100684", 3.79325, "532", "41", 7.70677)]
+    [InlineData("100019", 4.28844, 8.09859)]
+    [InlineData("100241", 4.51361, 10.02786)]
+    [InlineData("100353", 3.9508, 5.0)]
+    [InlineData("100448", 6.1856, 20.33898)]
+    [InlineData("100500", 6.44356, 19.87952)]
+    [InlineData("100674", 5.16147, 16.90141)]
+    [InlineData("100684", 3.79325, 7.70677)]
     public async Task AttendanceData_Expected(
         string urn, 
         double sess_overall_percent, 
-        string enrolments, 
-        string enrolments_pa_10_exact, 
         double enrolments_pa_10_exact_percent)
     {
         // Arrange && Act
