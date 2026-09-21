656e11523ec5c6110ff2e32715d5af4f28231b88
CLAWLOR <catherine.lawlor@education.gov.uk>
2026-09-21 12:29:52 +0100

    fix data and change the way its loaded in the tests. Complete breakdown establishment data testing

--- Tests/SAPPub.Integration.Tests/SecondarySchoolTests/AdditionalMeasuresPageTests.cs
+++ Tests/SAPPub.Integration.Tests/SecondarySchoolTests/AdditionalMeasuresPageTests.cs
@@ -10,36 +10,66 @@ public class AdditionalMeasuresPageTests : BasePageTest
     private string BasePageUrl(string urn) => $"/school/{urn}";
     private string pageUnderTest => "secondary-performance/additional-measures";
 
+    private static readonly IDictionary<string, PerformanceTablesTestDataModel> _performanceTotalsTestData = TestDataLoader.Load<PerformanceTablesTestDataModel>(
+        "KS4",
+        "202425_performance_tables_schools_final_Total_Current_Year").ToDictionary(x => x.SchoolUrn);
+
+    private static readonly IDictionary<string, PerformanceTablesTestDataModel> _performanceDisadvantagedData = TestDataLoader.Load<PerformanceTablesTestDataModel>(
+        "KS4",
+        "202425_performance_tables_schools_final_Disadvantaged_Current_Year").ToDictionary(x => x.SchoolUrn);
+
+    private static readonly IDictionary<string, PerformanceTablesTestDataModel> _performanceBoysData = TestDataLoader.Load<PerformanceTablesTestDataModel>(
+        "KS4",
+        "202425_performance_tables_schools_final_Boys_Current_Year").ToDictionary(x => x.SchoolUrn);
+
+    private static readonly IDictionary<string, PerformanceTablesTestDataModel> _performanceGirlsData = TestDataLoader.Load<PerformanceTablesTestDataModel>(
+        "KS4",
+        "202425_performance_tables_schools_final_Girls_Current_Year").ToDictionary(x => x.SchoolUrn);
+
+    private static readonly IDictionary<string, PerformanceTablesTestDataModel> _performanceEALData = TestDataLoader.Load<PerformanceTablesTestDataModel>(
+        "KS4",
+        "202425_performance_tables_schools_final_EAL_Current_Year").ToDictionary(x => x.SchoolUrn);
+
+    private static readonly IDictionary<string, PerformanceTablesTestDataModel> _performanceNonMobileData = TestDataLoader.Load<PerformanceTablesTestDataModel>(
+        "KS4",
+        "202425_performance_tables_schools_final_NonMobile_Current_Year").ToDictionary(x => x.SchoolUrn);
+
+    private static readonly IDictionary<string, InformationAboutSchoolsTestDataModel> _wholeSchoolTestData = TestDataLoader.Load<InformationAboutSchoolsTestDataModel>(
+        "KS4",
+        "202425_information_about_schools_final").ToDictionary(x => x.SchoolUrn);
+
     public record AdditionalMeasuresTestData
     {
         public required PerformanceTablesTestDataModel Totals { get; init; }
+        public required PerformanceTablesTestDataModel Boys { get; init; }
+        public required PerformanceTablesTestDataModel Girls { get; init; }
+        public required PerformanceTablesTestDataModel EAL { get; init; }
+        public required PerformanceTablesTestDataModel NonMobile { get; init; }
         public required PerformanceTablesTestDataModel Disadvantaged { get; init; }
     }
 
     public static TheoryData<AdditionalMeasuresTestData> GetAdditionalMeasuresData()
     {
-        var performanceTotalsData = TestDataLoader.Load<PerformanceTablesTestDataModel>(
-            "KS4",
-            "202425_performance_tables_schools_final_Total_Current_Year");
+        var urns = _performanceTotalsTestData.Keys
+            .Intersect(_performanceBoysData.Keys)
+            .Intersect(_performanceGirlsData.Keys)
+            .Intersect(_performanceEALData.Keys)
+            .Intersect(_performanceNonMobileData.Keys)
+            .Intersect(_performanceDisadvantagedData.Keys);
 
-        var performanceDisadvantagedData = TestDataLoader.Load<PerformanceTablesTestDataModel>(
-            "KS4",
-            "202425_performance_tables_schools_final_Disadvantaged_Current_Year");
-
-        var joinedData = performanceTotalsData.Join(
-            performanceDisadvantagedData,
-            totals => totals.SchoolUrn,
-            disadvantaged => disadvantaged.SchoolUrn,
-            (totals, disadvantaged) => new AdditionalMeasuresTestData()
-            {
-                Totals = totals,
-                Disadvantaged = disadvantaged
-            });
-
-        if (!joinedData.Any())
+        if (!urns.Any())
         {
             throw new InvalidOperationException("No matching test data found for test.");
         }
+        var joinedData = urns.Select(urn => new AdditionalMeasuresTestData()
+            {
+                Totals = _performanceTotalsTestData[urn],
+                Boys = _performanceBoysData[urn],
+                Girls = _performanceGirlsData[urn],
+                EAL = _performanceEALData[urn],
+                NonMobile = _performanceNonMobileData[urn],
+                Disadvantaged = _performanceDisadvantagedData[urn]
+            });
 
         return new TheoryData<AdditionalMeasuresTestData>(joinedData.ToArray());
     }
@@ -53,21 +83,18 @@ public class AdditionalMeasuresPageTests : BasePageTest
         Assert.NotNull(response);
         var _ = await Page.GotoPage(response.Url, pageUnderTest);
         await Page.ExpandAccordionByIdAsync("average-numexams-entered-by-pupil-char");
+        await Page.ExpandAccordionByIdAsync("further-pop-data-eoks4");
 
         // Assert
         // only establishment-level data is checked so far
         await AssertAverageNumberOfExamsEnteredPerPupilSection(testData.Totals, testData.Disadvantaged);
         await AssertAdditionalEntryAndAchievementMeasuresSection(testData.Totals);
-        await AssertNumberOfPupilsAtTheEndOfKS4Section(testData.Totals);
+        await AssertNumberOfPupilsAtTheEndOfKS4Section(testData);
     }
 
     public static TheoryData<InformationAboutSchoolsTestDataModel> GetWholeSchoolData()
     {
-        var wholeSchoolData = TestDataLoader.Load<InformationAboutSchoolsTestDataModel>(
-            "KS4",
-            "202425_information_about_schools_final"
-            );
-        return new TheoryData<InformationAboutSchoolsTestDataModel>(wholeSchoolData);
+        return new TheoryData<InformationAboutSchoolsTestDataModel>(_wholeSchoolTestData.Values.ToArray());
     }
 
     [Theory]
@@ -81,12 +108,7 @@ public class AdditionalMeasuresPageTests : BasePageTest
         await Page.ExpandAccordionByIdAsync("further-pop-data-wholeschool");
 
         // Assert
-        // only establishment-level data is checked so far
-        // not testing table "num-pupil-wholsechool-table", "Number of pupils on roll" because it's GIAS data and should be E2E tested on About the school page eventually
-        var pupilsWithSENSupport = await Page.GetTableRowValuesAsync("num-pupil-whole-school-sen-table", "Pupils with SEN support");
-        AssertHelpers.AssertNumericEqual(testData.SenNoEhcpPupilPercent, pupilsWithSENSupport[0]);
-        var pupilsWithEHCP = await Page.GetTableRowValuesAsync("num-pupil-whole-school-ehcp-table", "Pupils with EHCPs");
-        AssertHelpers.AssertNumericEqual(testData.SenWithEhcpPupilPercent, pupilsWithEHCP[0]);
+        await AssertNumberOfPupilsAtTheWholeSchoolSection(testData);
     }
 
     private async Task AssertAverageNumberOfExamsEnteredPerPupilSection(PerformanceTablesTestDataModel testCase1, PerformanceTablesTestDataModel testCase2)
@@ -100,6 +122,8 @@ public class AdditionalMeasuresPageTests : BasePageTest
         AssertHelpers.AssertNumericEqual(testCase2.GcseEntriesAverage, gcseDataDisadvantaged[0]);
         var allQualificationsDataDisadvantaged = await Page.GetTableRowValuesAsync("additional-measures-exams-entered-disadvantaged-table", "All KS4 qualifications");
         AssertHelpers.AssertNumericEqual(testCase2.QualEntriesAverage, allQualificationsDataDisadvantaged[0]);
+
+        // LA and England non-disadvantaged data is not yet tested here
     }
 
     private async Task AssertAdditionalEntryAndAchievementMeasuresSection(PerformanceTablesTestDataModel testCase)
@@ -112,15 +136,30 @@ public class AdditionalMeasuresPageTests : BasePageTest
         AssertHelpers.AssertNumericEqual(testCase.LanMultipleEnteringPercent, tableContent[0]);
     }
 
-    private async Task AssertNumberOfPupilsAtTheEndOfKS4Section(PerformanceTablesTestDataModel testCase)
+    private async Task AssertNumberOfPupilsAtTheEndOfKS4Section(AdditionalMeasuresTestData testCase)
     {
         var tableContent = await Page.GetTableRowValuesAsync("num-pupil-eofks4-table", "Number of pupils at the end of KS4");
-        AssertHelpers.AssertNumericEqual(testCase.PupilCount, tableContent[0]);
+        AssertHelpers.AssertNumericEqual(testCase.Totals.PupilCount, tableContent[0]);
 
-        // TODO check all the values in the breakdown tabel, which involves loading and joining data for additional files 
-        // 202425_performance_tables_schools_final_Boys_Current_Year,
-        // 202425_performance_tables_schools_final_Girls_Current_Year,
-        // 202425_performance_tables_schools_final_EAL_Current_Year,
-        // 202425_performance_tables_schools_final_NonMobile_Current_Year
+        var tableContentGirls = await Page.GetTableRowValuesAsync("num-pupil-eofks4-breakdown-table", "Girls");
+        AssertHelpers.AssertNumericEqual(testCase.Girls.PupilCount, tableContentGirls[0]);
+        var tableContentBoys = await Page.GetTableRowValuesAsync("num-pupil-eofks4-breakdown-table", "Boys");
+        AssertHelpers.AssertNumericEqual(testCase.Boys.PupilCount, tableContentBoys[0]);
+        var tableContentEAL = await Page.GetTableRowValuesAsync("num-pupil-eofks4-breakdown-table", "English as an additional language (EAL)");
+        AssertHelpers.AssertNumericEqual(testCase.EAL.PupilCount, tableContentEAL[0]);
+        var tableContentNonMobile = await Page.GetTableRowValuesAsync("num-pupil-eofks4-breakdown-table", "Non-mobile pupils");
+        AssertHelpers.AssertNumericEqual(testCase.NonMobile.PupilCount, tableContentNonMobile[0]);
+        var tableContentDisadvantaged = await Page.GetTableRowValuesAsync("num-pupil-eofks4-disadvantaged-table", "Number of disadvantaged pupils");
+        AssertHelpers.AssertNumericEqual(testCase.Disadvantaged.PupilCount, tableContentDisadvantaged[0]);
+    }
+
+    private async Task AssertNumberOfPupilsAtTheWholeSchoolSection(InformationAboutSchoolsTestDataModel testCase)
+    {
+        // only establishment-level data is checked so far
+        // not testing table "num-pupil-wholeschool-table", "Number of pupils on roll" because it's GIAS data and should be E2E tested on About the school page eventually
+        var pupilsWithSENSupport = await Page.GetTableRowValuesAsync("num-pupil-whole-school-sen-table", "Pupils with SEN support");
+        AssertHelpers.AssertNumericEqual(testCase.SenNoEhcpPupilPercent, pupilsWithSENSupport[0]);
+        var pupilsWithEHCP = await Page.GetTableRowValuesAsync("num-pupil-whole-school-ehcp-table", "Pupils with EHCPs");
+        AssertHelpers.AssertNumericEqual(testCase.SenWithEhcpPupilPercent, pupilsWithEHCP[0]);
     }
 }
