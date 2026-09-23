c9bf8be4bb683020418507c39c6e29282c8b48fe
CLAWLOR <catherine.lawlor@education.gov.uk>
2026-09-23 13:19:15 +0100

    blank test class

---
+++ Tests/SAPPub.Integration.Tests/SecondarySchoolTests/EnglishAndMathsPageTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SAPPub.Integration.Tests.SecondarySchoolTests;
+
+public class EnglishAndMathsPageTests : BasePageTest
+{
+    [Theory]
+    [MemberData(nameof(GetGrade5AndAboveData))]
+    public async Task Grade5AndAboveData_Expected()
+    {
+
+    }
+}
